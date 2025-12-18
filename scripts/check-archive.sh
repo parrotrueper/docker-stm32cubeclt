194bea961517c7ac5eff402048441a62e883d5e0
deya <deya.sanchez@gmail.com>
2025-12-18 12:19:49 +0000

    chore: shellcheck linter changes

--- scripts/check-archive.sh
+++ scripts/check-archive.sh
@@ -14,15 +14,15 @@ set -o pipefail
 # - checksum value
 # - filename
 function CHECKMD5() {
-  md5_to_test=$1
-  md5_from_file=$(md5sum "$2" | cut -d " " -f1)
-  md5_results="CSUM reference($md5_to_test) CSUM of File($md5_from_file)"
-  if [[ $md5_to_test == "$md5_from_file" ]]; then
-      info "$md5_results"
-    else
-      fatal 1 "$md5_results"
-  fi
+	md5_to_test=$1
+	md5_from_file=$(md5sum "$2" | cut -d " " -f1)
+	md5_results="CSUM reference(${md5_to_test}) CSUM of File(${md5_from_file})"
+	if [[ ${md5_to_test} == "${md5_from_file}" ]]; then
+		info "${md5_results}"
+	else
+		fatal 1 "${md5_results}"
+	fi
 }
 
 info "Checking MD5SUM of archive"
-run CHECKMD5  "${CSUM}" "./${ST_ARCHIVE}"
+run CHECKMD5 "${CSUM:?}" "./${ST_ARCHIVE:?}"
--- scripts/installer.sh
+++ scripts/installer.sh
@@ -9,21 +9,21 @@ trap 'printf "\n\nERROR at $0 line $LINENO. Exiting.\n\n"' ERR
 st_sh_file=$1
 
 tmp_dir="/tmp/st"
-mkdir -p ${tmp_dir}
+mkdir -p "${tmp_dir}"
 
 chmod +x "${st_sh_file}"
 
 /usr/bin/bash "${st_sh_file}" --tar -xvf -C "${tmp_dir}/"
-cd ${tmp_dir}
+cd "${tmp_dir}"
 
 # installation needs to be as root
-LICENSE_ALREADY_ACCEPTED=1 /usr/bin/bash ${tmp_dir}/setup.sh
+LICENSE_ALREADY_ACCEPTED=1 /usr/bin/bash "${tmp_dir}"/setup.sh
 
 # shellcheck disable=2028
-echo "printf \"\n\nST tools can be found in /opt/st/\n\n\"" >> /etc/bash.bashrc
+echo "printf \"\n\nST tools can be found in /opt/st/\n\n\"" >>/etc/bash.bashrc
 
 cd /
 
 # cleanup
-rm -rf ${tmp_dir}
+rm -rf "${tmp_dir}"
 rm -rf "${st_sh_file}"
